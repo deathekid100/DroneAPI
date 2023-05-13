3e120bd62cecc08d3b4ab90d789cdfd632d6b181
Ernesto Torres Ojea <ernesto.torres@ntsprint.com>
2023-05-13 01:12:09 -0400

    Add Repository and UnitofWork pattern

---
+++ DronesAPI/Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using DronesAPI.Models;
+
+namespace DronesAPI.Data
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly DroneDBContext _context;
+
+        public UnitOfWork(DroneDBContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            DroneRepository ??= new GenericRepository<Drone>(_context);
+            MedicationRepository ??= new GenericRepository<Medication>(_context);
+        }
+
+        public IGenericRepository<Drone> DroneRepository { get; set; }
+        public IGenericRepository<Medication> MedicationRepository { get; set; }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
